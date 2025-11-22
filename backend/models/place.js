d172c711d7deaae5f6a169d87120791ec9013994
Minh <voq@sheridancollege.ca>
2025-11-21 20:14:17 -0500

    updated schema

--- backend/models/place.js
+++ backend/models/place.js
@@ -1,34 +1,25 @@
-// backend/models/place.js
 const mongoose = require("mongoose");
 
 const placeSchema = new mongoose.Schema(
   {
-    // A. Dish & location
     dishName: { type: String, required: true },
 
     locationCity: { type: String, required: true },
     locationCountry: { type: String, required: true },
 
-    placeType: { type: String }, // Street Food, Cafe, Restaurant
-
-    // B. Rating & price
+    placeType: { type: String },
     rating: { type: Number, min: 1, max: 5, required: true },
-    priceLevel: { type: String }, // $, $$, $$$
+    priceLevel: { type: String },
 
-    // C. Tags & date
     KeywordTags: { type: [String], default: [] },
     visitDate: { type: Date, required: true },
 
-    // D. Notes & photo
     notes: { type: String },
     photoUrl: { type: String },
 
-    // optional flag, not used in UI yet
     visited: { type: Boolean, default: true },
   },
-  {
-    timestamps: true, // adds createdAt / updatedAt
-  }
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Place", placeSchema);
