13772a32304b616d90b4567b405092f8a4223fb0
Minh <voq@sheridancollege.ca>
2025-12-13 14:27:54 -0500

    Use PORT env for Render

--- backend/server.js
+++ backend/server.js
@@ -69,7 +69,9 @@ app.delete("/places/:id", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
